e3d6ac726dde6b7714af0e555277baa5142a1756
Homer LeMar <homerlemar@gmail.com>
2017-12-17 21:16:32 -0600

    Converting to jest.

--- test/unit/specs/Cell.spec.js
+++ test/unit/specs/Cell.spec.js
@@ -1,31 +1,28 @@
-/* eslint-disable */ 
-import Vue from 'vue'
-import Cell from '@/components/Cell'
+import Vue from 'vue';
+import Cell from '@/components/Cell';
 
 describe('Cell.vue', () => {
-
   let Constructor;
   beforeEach(() => {
     Constructor = Vue.extend(Cell);
   });
   it('should render contents as a div', () => {
     const vm = new Constructor().$mount();
-    expect(vm.$el.nodeName).to.equal('DIV');
+    expect(vm.$el.nodeName).toEqual('DIV');
   });
 
   it('should render correct contents as ""', () => {
     const vm = new Constructor().$mount();
 
     expect(vm.$el.textContent)
-    .to.equal('')
+    .toEqual('');
   });
 
   it('should render correct contents as X', () => {
-    const propsData = {token: 'X'};
+    const propsData = { token: 'X' };
     const vm = new Constructor({ propsData }).$mount();
 
     expect(vm.$el.textContent)
-    .to.equal('X')
+    .toEqual('X');
   });
-
-})
+});
