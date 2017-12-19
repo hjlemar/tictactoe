ab95692d291ac6eec11d0dceb29990972c18134d
Homer LeMar <homerlemar@gmail.com>
2017-12-19 17:20:12 -0600

    Creating Info component

---
+++ src/components/Info.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="col-sm-6">
+      <div class="alert-info alert">{{ status }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['status'],
+};
+</script>
+
+<style>
+
+</style>
